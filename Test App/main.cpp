928e08f182ffe9af40bfd4727ab328f10f23986e
moneyl <8206401+Moneyl@users.noreply.github.com>
2020-08-10 22:19:06 -0400

    Save partial chunk file reader
    Can read the render data (mesh format + material data) since that's already pretty well understood, but haven't gotten started on the destruction data or collision meshes.

--- Test App/main.cpp
+++ Test App/main.cpp
@@ -4,39 +4,203 @@
 #include "Common/filesystem/File.h"
 #include "RfgTools++/formats/packfiles/Packfile3.h"
 #include <iostream>
+#include <RfgTools++\formats\meshes\MeshDataBlock.h>
 
 int main()
 {
-    string inputPath = "G:/RFG Unpack/data/misc.vpp_pc";
-    string outputPath = "G:/RFG Unpack 2/CppToolOutput/Unpack/";
+    //string inputPath = "G:/RFG Unpack/data/misc.vpp_pc";
+    //string outputPath = "G:/RFG Unpack 2/CppToolOutput/Unpack/";
 
-    std::cout << "inputPath: \"" << inputPath << "\"\n";
-    std::cout << "outputPath: \"" << outputPath << "\"\n";
+    //std::cout << "inputPath: \"" << inputPath << "\"\n";
+    //std::cout << "outputPath: \"" << outputPath << "\"\n";
 
-    u32 numRuns = 1; //Should use higher number for more precise results. Need speed for testing though
-    std::vector<u64> times;
-    times.reserve(numRuns);
+    //u32 numRuns = 1; //Should use higher number for more precise results. Need speed for testing though
+    //std::vector<u64> times;
+    //times.reserve(numRuns);
 
-    printf("Extracting packfile %d times...\n", numRuns);
-    Timer timer(true);
+    //printf("Extracting packfile %d times...\n", numRuns);
+    //Timer timer(true);
 
-    for (u32 i = 0; i < numRuns; i++)
+    //for (u32 i = 0; i < numRuns; i++)
+    //{
+    //    Packfile3 packfile(inputPath);
+    //    packfile.ReadMetadata();
+    //    packfile.ExtractSubfiles(outputPath + Path::GetFileNameNoExtension(inputPath) + "/");
+    //    //auto weaponsXtbl = packfile.ExtractSingleFile("weapons.xtbl");
+    //    //File::WriteToFile("G:\\RFG Unpack\\boop.xml", weaponsXtbl.value());
+    //    //delete[] weaponsXtbl.value().data();
+    //    times.push_back(timer.ElapsedMicroseconds());
+    //    timer.Reset();
+    //}
+
+    //f32 sum = 0.0f;
+    //for (auto& val : times)
+    //    sum += (f32)val;
+
+    //std::cout << "Elapsed time (average over " << numRuns << " runs): " << sum / (f32)numRuns << "us | " << sum / (f32)numRuns / 1000.0f << "ms | " << sum / (f32)numRuns / 1000000.0f << "s\n";
+
+    struct ChunkHeader
     {
-        Packfile3 packfile(inputPath);
-        packfile.ReadMetadata();
-        packfile.ExtractSubfiles(outputPath + Path::GetFileNameNoExtension(inputPath) + "/");
-        //auto weaponsXtbl = packfile.ExtractSingleFile("weapons.xtbl");
-        //File::WriteToFile("G:\\RFG Unpack\\boop.xml", weaponsXtbl.value());
-        //delete[] weaponsXtbl.value().data();
-        times.push_back(timer.ElapsedMicroseconds());
-        timer.Reset();
+        unsigned int signature = 0;
+        unsigned int version = 0;
+        unsigned int source_rfgchunkx_version = 0;
+        unsigned int render_data_checksum = 0;
+        unsigned int render_cpu_data_offset = 0;
+        unsigned int render_cpu_data_size = 0;
+        unsigned int collision_model_checksum = 0;
+        unsigned int collision_model_data_offset = 0;
+        unsigned int collision_model_data_size = 0;
+        unsigned int destruction_checksum = 0;
+        unsigned int destruction_offset = 0;
+        unsigned int destruction_datasize = 0;
+    };
+
+    string cpuFilePath = "G:/RFG Unpack/data/Unpack/terr01_l1.vpp_pc/Unpack/0h_c2337.str2_pc/0202tower_guard.cchk_pc";
+    string gpuFilePath = "G:/RFG Unpack/data/Unpack/terr01_l1.vpp_pc/Unpack/0h_c2337.str2_pc/0202tower_guard.gchk_pc";
+
+    BinaryReader cpuFile(cpuFilePath);
+    BinaryReader gpuFile(gpuFilePath);
+
+    ChunkHeader header;
+    cpuFile.ReadToMemory((void*)&header, sizeof(ChunkHeader));
+    std::cout << "Position in cpu file after reading header: " << cpuFile.Position() << "\n";
+
+    cpuFile.Skip(400); //Skip 400 unknown / possibly padding bytes
+    u32 unkValue0 = cpuFile.ReadUint32();
+    cpuFile.Skip(28);
+    u32 unkValue1 = cpuFile.ReadUint32();
+    cpuFile.Skip(32);
+    u32 unkValue2 = cpuFile.ReadUint32();
+    cpuFile.Skip(184);
+
+    //Should be at the render data offset
+    if (cpuFile.Position() != header.render_cpu_data_offset)
+        throw std::exception("Error! Expected chunk render data offset does not match expected value!");
+
+    std::cout << "Position at start of chunk render data block: " << cpuFile.Position() << "\n";
+    
+    //Read the mesh data block that should be at the start of the render data offset
+    MeshDataBlock meshData;
+    meshData.Read(cpuFile);
+
+    std::cout << "Position at end of chunk render data MeshDataBlock: " << cpuFile.Position() << "\n";
+
+    //Align to 16 bytes and read size of texture names block
+    cpuFile.Align(16);
+    u32 textureNamesBlockSize = cpuFile.ReadUint32();
+
+    //Read texture names block. These are double null terminated
+    std::vector<string> textureNames;
+    u64 textureNamesStartPos = cpuFile.Position();
+    //Read strings until we reach the end of the string block
+    while (cpuFile.Position() - textureNamesStartPos < textureNamesBlockSize)
+    {
+        auto pos0 = cpuFile.Position();
+        //Read null terminated string
+        textureNames.push_back(cpuFile.ReadNullTerminatedString());
+        auto pos1 = cpuFile.Position();
+        //Skip any additional null terminators. RFG likes to have random amounts of these
+        while (cpuFile.Position() - textureNamesStartPos < textureNamesBlockSize)
+        {
+            auto pos2 = cpuFile.Position();
+            if (cpuFile.PeekChar() == '\0')
+                cpuFile.Skip(1);
+            else
+                break;
+        }
+    }
+    cpuFile.Align(16);
+
+    std::cout << "Position at end of texture names block: " << cpuFile.Position() << "\n";
+
+    //Todo: Update this comment if I figure this trash out
+    //Some kind of material data. Haven't found a generic material data structure used across mesh types yet so this may be tricky
+    u32 materialOffset = cpuFile.ReadUint32();
+    u32 numMaterials = cpuFile.ReadUint32();
+    std::vector<u32> materialIds; //Todo: Figure out what these really are
+    for (u32 i = 0; i < numMaterials; i++)
+        materialIds.push_back(cpuFile.ReadUint32());
+
+    //I dunno what this even does but the game seems to do something roughly analogous to this
+    cpuFile.Skip(materialOffset); //Todo: Figure out what the hell is going on with this
+    cpuFile.Skip(numMaterials * 8); //The game does this for some reason
+    cpuFile.Align(16); //Todo: Make sure this reliably gets us to the next chunk of data
+
+
+
+    struct rl_material_data
+    {
+        unsigned int shader_handle;
+        unsigned int name_checksum;
+        unsigned int mat_flags;
+        unsigned __int16 num_textures;
+        char num_constants;
+        char max_constants;
+        u32 textures_offset;
+        u32 constant_name_checksum_offset;
+        u32 constant_block_offset;
+    };
+    struct texture_desc
+    {
+        u32 name_offset;
+        u32 name_checksum;
+        u32 texture_index;
+    };
+    struct material_constant
+    {
+        float constants[4];
+    };
+    struct material_data_block
+    {
+        rl_material_data material_data;
+        std::vector<texture_desc> texture_descs;
+        std::vector<u32> constant_name_checksums;
+        std::vector<material_constant> constants;
+
+        void Read(BinaryReader& reader)
+        {
+            u64 startPos = reader.Position();
+            u32 materialDataSize = reader.ReadUint32();
+
+            reader.ReadToMemory(&material_data, sizeof(rl_material_data));
+            for (u32 i = 0; i < material_data.num_textures; i++)
+            {
+                auto& desc = texture_descs.emplace_back();
+                reader.ReadToMemory(&desc, sizeof(texture_desc));
+            }
+            for (u32 i = 0; i < material_data.num_constants; i++)
+            {
+                constant_name_checksums.push_back(reader.ReadUint32());
+            }
+            reader.Align(16);
+            for (u32 i = 0; i < material_data.max_constants; i++)
+            {
+                auto& constantData = constants.emplace_back();
+                reader.ReadToMemory(&constantData, sizeof(material_constant));
+            }
+
+            if (reader.Position() - startPos != materialDataSize)
+                throw std::exception("Error! Failed to read material data block. Length of read data doesn't equal expected data size.");
+        }
+    };
+
+    std::vector<material_data_block> materials;
+
+    for (u32 i = 0; i < numMaterials; i++)
+    {
+        cpuFile.Align(16);
+        auto& mat = materials.emplace_back();
+        mat.Read(cpuFile);
     }
 
-    f32 sum = 0.0f;
-    for (auto& val : times)
-        sum += (f32)val;
+    std::cout << "Position at end of material data block 0: " << cpuFile.Position() << "\n";
+    //Todo: Figure out if supposed to Align(64) or Skip(64) here. Game seems to Align(64) but Skip(64) is what gets us to the next data section in the test file
+    //cpuFile.Align(64);
+    cpuFile.Skip(64);
+    std::cout << "Position at expected start of destruction data block: " << cpuFile.Position() << "\n";
 
-    std::cout << "Elapsed time (average over " << numRuns << " runs): " << sum / (f32)numRuns << "us | " << sum / (f32)numRuns / 1000.0f << "ms | " << sum / (f32)numRuns / 1000000.0f << "s\n";
+    if (cpuFile.Position() != header.destruction_offset)
+        throw std::exception("Error! Current location does not equal the destruction data offset as it is expected to at this point!");
 
     return 0;
 }
