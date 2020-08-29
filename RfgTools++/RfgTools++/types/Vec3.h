72339bea22609039ef61f31b9b04239c40a5f3d8
moneyl <8206401+Moneyl@users.noreply.github.com>
2020-08-29 01:21:02 -0400

    Add more operators to Vec3

--- RfgTools++/RfgTools++/types/Vec3.h
+++ RfgTools++/RfgTools++/types/Vec3.h
@@ -17,6 +17,12 @@ struct Vec3
         return Vec3{ x + B.x, y + B.y, z + B.z };
     }
 
+    Vec3 operator+=(const Vec3& B)
+    {
+        *this = *this + B;
+        return *this;
+    }
+
     Vec3 operator/(f32 scalar)
     {
         return Vec3{ x / scalar, y / scalar, z / scalar };
@@ -25,5 +31,24 @@ struct Vec3
     f32 Distance(const Vec3& B) const
     {
         return sqrt(pow(B.x - x, 2) + pow(B.y - y, 2) + pow(B.z - z, 2));
+    }
+
+    Vec3 Cross(const Vec3& B)
+    {
+        Vec3 C;
+        C.x = (y * B.z) - (B.y * z);
+        C.y = (B.x * z) - (y * B.z);
+        C.z = (x * B.y) - (B.x * y);
+        return C;
+    }
+
+    f32 Magnitude()
+    {
+        return sqrtf((x * x) + (y * y) + (z * z));
+    }
+
+    Vec3 Normalize()
+    {
+        return *this / Magnitude();
     }
 };
