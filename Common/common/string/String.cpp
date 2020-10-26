7bf999085f9b5ae81d8eb67e16cacea4093a5c3d
moneyl <8206401+Moneyl@users.noreply.github.com>
2020-10-26 19:46:14 -0400

    Add String::Contains to common lib

--- Common/common/string/String.cpp
+++ Common/common/string/String.cpp
@@ -16,6 +16,11 @@ namespace String
         return value.compare(0, start.length(), start) == 0;
     }
 
+    bool Contains(s_view value, s_view target)
+    {
+        return value.find(target) != std::string_view::npos;
+    }
+
     string ToLower(const string& value)
     {
         std::string Copy = value; //Copy the string since we want to leave the original string intact
--- Common/common/string/String.h
+++ Common/common/string/String.h
@@ -5,5 +5,6 @@ namespace String
 {
     bool EndsWith(s_view value, s_view ending);
     bool StartsWith(s_view value, s_view start);
+    bool Contains(s_view value, s_view target);
     string ToLower(const string& value);
 }
